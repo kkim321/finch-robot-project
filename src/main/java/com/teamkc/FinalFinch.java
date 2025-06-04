4cc553f446581c8940ba2fc6188d4cc17f781dc3
kkim321 <kkim0041@mymail.lausd.net>
2025-06-04 10:35:00 -0700

    update code

--- src/main/java/com/teamkc/FinalFinch.java
+++ src/main/java/com/teamkc/FinalFinch.java
@@ -1,5 +1,25 @@
-import java.util.Scanner;
+package com.teamkc;
+
 import com.birdbrain.Finch;
 
-public class FinalFinch
+public class FinalFinch {
+    public static void main(String[] args) {
+        Finch bird = new Finch();
+        bird.playNote(60,0.5);
+   
+        bird.setTurn("L", 18, 50);
+        bird.setMove("F",12.5,50);
+       
 
+        for (int i = 0; i < 4; i++) {
+            bird.setTurn("R", 144, 50);
+            bird.setMove("F",12.5,50);
+            
+        }
+
+            
+
+        bird.stop();
+        bird.disconnect();
+    }
+}
